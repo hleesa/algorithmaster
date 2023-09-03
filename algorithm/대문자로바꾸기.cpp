006789ac59fe437310ad473856960d985dc7d7a3
salee2 <hleesa>
2023-09-03 17:13:59 +0900

    코딩 기초 트레이닝 대문자로 바꾸기

--- algorithm/대문자로바꾸기.cpp
+++ algorithm/대문자로바꾸기.cpp
@@ -1,1 +1,11 @@
+#include <string>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+string solution(string myString) {
+    string answer;
+    transform(myString.begin(), myString.end(), back_inserter(answer), ::toupper);
+    return answer;
+}
