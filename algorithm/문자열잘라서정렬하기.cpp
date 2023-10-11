9426addb749283bcf94887c304571f8237612dd3
salee2 <hleesa>
2023-10-11 17:04:46 +0900

    코딩 기초 트레이닝 문자열 잘라서 정렬하기

---
+++ algorithm/문자열잘라서정렬하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+vector<string> solution(string myString) {
+    vector<string> answer;
+    istringstream ss(myString);
+    string token;
+    while (getline(ss, token, 'x')) {
+        if (token.empty()) continue;
+        answer.push_back(token);
+    }
+    sort(answer.begin(), answer.end());
+    return answer;
+}
