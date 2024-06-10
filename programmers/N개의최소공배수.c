523e11406e9d7f4750f1f89a6b560713ea7c49c0
lsh <salee2@student.42seoul.kr>
2024-06-10 14:57:25 +0900

    feat: 연습문제 > N개의 최소공배수

--- programmers/N개의최소공배수.c
+++ programmers/N개의최소공배수.c
@@ -2,9 +2,9 @@
 
 
 //v2
-int gcd2(int a, int b){
+int gcd2(int a, int b) {
     int c;
-    while(b){
+    while (b) {
         c = a % b;
         a = b;
         b = c;
@@ -12,13 +12,13 @@ int gcd2(int a, int b){
     return a;
 }
 
-int lcm(int a, int b){
-    return a * (b / gcd2(a,b));
+int lcm(int a, int b) {
+    return a * (b / gcd2(a, b));
 }
 
 int solution2(int arr[], size_t arr_len) {
     int ans = arr[0];
-    for(size_t i = 1; i < arr_len; ++i){
+    for (size_t i = 1; i < arr_len; ++i) {
         ans = lcm(ans, arr[i]);
     }
     return ans;
